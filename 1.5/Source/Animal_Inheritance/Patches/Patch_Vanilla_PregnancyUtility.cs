66cda068af2de05a2c07c1a5abfe77d5cec899d2
boredom974 <boredom974@gmail.com>
2024-12-22 17:37:21 -0500

    Added Prefix to GetInheritedGeneSet
    replaces the animal parent parameter with a dummy pawn that has all the animal genes added to them

--- 1.5/Source/Animal_Inheritance/Patches/Patch_Vanilla_PregnancyUtility.cs
+++ 1.5/Source/Animal_Inheritance/Patches/Patch_Vanilla_PregnancyUtility.cs
@@ -21,6 +21,39 @@ namespace RJW_BGS
     )]
     public static class Patch_Vanilla_PregnancyUtility
     {
+        [HarmonyPriority(int.MaxValue)]
+        [HarmonyPrefix]
+        public static void VanillaGeneInheritance(ref Pawn father, ref Pawn mother)
+        {
+            if (!RJW_BGSSettings.rjw_bgs_enabled || !RJW_BGSSettings.rjw_bgs_vanilla_inheritance)
+            {
+                return;
+            }
+
+            if (mother.RaceProps.Humanlike != father.RaceProps.Humanlike)
+            {
+                Pawn animalParent = PawnGenerator.GeneratePawn(PawnKindDefOf.Colonist); //can make random xenotype so needs to be stripped of genes. I dont wanna deal with PawnGenerationRequests right now.
+                animalParent.genes.SetXenotype(XenotypeDefOf.Baseliner);
+                for (int i = animalParent.genes.Endogenes.Count - 1; i >= 0; i--)
+                {
+                    animalParent.genes.RemoveGene(animalParent.genes.Endogenes[i]);
+                }
+
+                if (!father.RaceProps.Humanlike)
+                {
+                    RJW_Genes.ModLog.Debug($"Father was found to be animal - looking up genes for {father.Name} and applying them to dummy pawn in his place.");
+                    InheritanceUtility.AddGenes(animalParent, InheritanceUtility.SelectAllGenes(father));
+                    father = animalParent;
+                }
+                else
+                {
+                    RJW_Genes.ModLog.Debug($"Mother was found to be animal - looking up genes for {mother.Name} and applying them to dummy pawn in her place.");
+                    InheritanceUtility.AddGenes(animalParent, InheritanceUtility.SelectAllGenes(mother));
+                    mother = animalParent;
+                }
+            }
+        }
+        
         [HarmonyPostfix]
         public static void AnimalInheritedGenes(Pawn father, Pawn mother, ref GeneSet __result)
         {
